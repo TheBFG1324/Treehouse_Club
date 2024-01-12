2c7fe3c951417c0f4b5d3e5be39786fb20a9ed5c
CameronDenton <cwdenton5@gmail.com>
2024-01-12 16:34:59 -0600

    added server functions

--- server/Server.js
+++ server/Server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const crypto = require('crypto');
 
 const app = express();
 app.use(express.json()); 
@@ -20,9 +21,140 @@ MongoClient.connect(mongoUri)
         console.error('Error connecting to MongoDB', err);
     });
 
+async function getAccount(object){
+    return await collection.findOne(object);
+}
 
-app.get('/api/items', async (req, res) => {
+    app.post('/api/create-account', async (req, res) => {
+        try {
+            const googleId = req.body.googleId;
+            const email = req.body.email;
+            const publicName = req.body.publicName;
+            const anonymousName = crypto.createHash('sha256').update(publicName).digest('hex').slice(24, 40);
+            const accountImage = 1;
+    
+            if (!googleId || !email || !publicName || !accountImage) {
+                return res.status(400).json({ message: 'Missing or invalid parameters in request' });
+            }
+
+            if( await getAccount({googleId: googleId})){
+                return res.status(400).json({ message: 'already has an account' });
+            }
+    
+            const newItem = {
+                googleId: googleId,
+                email: email,
+                publicName: publicName,
+                anonymousName: anonymousName,
+                accountImage: accountImage,
+                publicPosts: [],
+                anonymousPosts: [],
+                followers: [],
+                following: [],
+                engagements: 0
+            };
+            
+            const result = await collection.insertOne(newItem);
+            res.status(200).json({ insertedId: result.insertedId, ...newItem });
+        } catch (error) {
+            res.status(500).json({ message: 'Error occurred while creating account', error });
+        }
+    });
+    
+
+
+app.get('/api/get-account-info', async (req, res) => {
     try {
+        const googleId = req.body.googleId;
+
+        if (!googleId) {
+            return res.status(400).json({ message: 'Missing or invalid googleId in request' });
+        }
+
+        const account = await getAccount({googleId: googleId});
+        res.status(200).json(account);
+
+    } catch (error) {
+        res.status(500).json({ message: 'Error occurred while fetching items', error });
+    }
+});
+
+app.get('/api/create-post', async (req, res) => {
+    try {
+        const itemData = req.body.key1;
+        console.log(itemData)
+        const items = await collection.find().toArray();
+        res.status(200).json(items);
+    } catch (error) {
+        res.status(500).json({ message: 'Error occurred while fetching items', error });
+    }
+    
+});
+
+app.get('/api/get-post', async (req, res) => {
+    try {
+        const itemData = req.body.key1;
+        console.log(itemData)
+        const items = await collection.find().toArray();
+        res.status(200).json(items);
+    } catch (error) {
+        res.status(500).json({ message: 'Error occurred while fetching items', error });
+    }
+    
+});
+
+app.get('/api/delete-post', async (req, res) => {
+    try {
+        const itemData = req.body.key1;
+        console.log(itemData)
+        const items = await collection.find().toArray();
+        res.status(200).json(items);
+    } catch (error) {
+        res.status(500).json({ message: 'Error occurred while fetching items', error });
+    }
+    
+});
+
+app.get('/api/engage-post', async (req, res) => {
+    try {
+        const itemData = req.body.key1;
+        console.log(itemData)
+        const items = await collection.find().toArray();
+        res.status(200).json(items);
+    } catch (error) {
+        res.status(500).json({ message: 'Error occurred while fetching items', error });
+    }
+    
+});
+
+app.get('/api/follow-unfollow', async (req, res) => {
+    try {
+        const itemData = req.body.key1;
+        console.log(itemData)
+        const items = await collection.find().toArray();
+        res.status(200).json(items);
+    } catch (error) {
+        res.status(500).json({ message: 'Error occurred while fetching items', error });
+    }
+    
+});
+
+app.get('/api/search-account', async (req, res) => {
+    try {
+        const itemData = req.body.key1;
+        console.log(itemData)
+        const items = await collection.find().toArray();
+        res.status(200).json(items);
+    } catch (error) {
+        res.status(500).json({ message: 'Error occurred while fetching items', error });
+    }
+    
+});
+
+app.get('/api/load-posts', async (req, res) => {
+    try {
+        const itemData = req.body.key1;
+        console.log(itemData)
         const items = await collection.find().toArray();
         res.status(200).json(items);
     } catch (error) {
