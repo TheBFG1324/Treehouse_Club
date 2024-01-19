b1f5c053ae3c094684be42daaa52a3e14fd8f5b6
CameronDenton <cwdenton5@gmail.com>
2024-01-18 19:42:12 -0600

    added error protection

--- server/Server.js
+++ server/Server.js
@@ -8,35 +8,55 @@ const crypto = require('crypto');
 const app = express();
 app.use(express.json()); 
 
-const mongoUri = 'mongodb://127.0.0.1:27017/learningDB'; // Simplified MongoDB URI
-const dbName = 'learningDB';
+const mongoUri = 'mongodb://127.0.0.1:27017/testDB'; // Simplified MongoDB URI
+const dbName = 'testDB';
 
 let db;
-let collection;
+let googleIdMapping;
+let accounts;
+let posts;
 
 MongoClient.connect(mongoUri)
     .then(client => {
-        console.log('Connected to MongoDB');
         db = client.db(dbName);
-        collection = db.collection('users');
+        accounts = db.collection('accounts');
+        posts = db.collection('posts');
+        googleIdMapping = db.collection('googleIdMapping');
+        console.log('Connected to MongoDB');
     })
     .catch(err => {
         console.error('Error connecting to MongoDB', err);
     });
 
-async function getAccount(object){
-    return await collection.findOne(object);
+async function checkIsEnrolled(googleId){
+
+    const result = await googleIdMapping.find({googleId: googleId}).toArray()
+    if(result.length != 0){
+        return true
+    }
+
+    return false
+}
+
+async function checkHasAccount(object){
+
+    const account = await accounts.findOne(object);
+    if(account){
+        return true
+    }
+
+    return  false
 }
 
 async function getPost(postId){
     if (!ObjectId.isValid(postId)) {
-        return { sucess: false, message: 'Invalid post ID' };
+        return false;
     }
 
     const objectId = new ObjectId(postId);
-    const post = await collection.findOne({_id: objectId});
+    const post = await posts.findOne({_id: objectId});
     if (!post) {
-        return { sucess: false, message: 'Post not found' };
+        return false
     }
 
     return post
@@ -45,102 +65,149 @@ async function getPost(postId){
 function areAllParametersValid(params) {
     return Object.values(params).every(param => param);
 }
-    app.post('/api/create-account', async (req, res) => {
-        try {
+app.post('/api/create-account', async (req, res) => {
+    try {
+        const { googleId, email, publicName, profileImage } = req.body;
+        const anonymousName = crypto.createHash('sha256').update(publicName).digest('hex').slice(24, 40);
 
-            const { googleId, email, publicName } = req.body;
-            const anonymousName = crypto.createHash('sha256').update(publicName).digest('hex').slice(24, 40);
-
-            
-            if (!areAllParametersValid({ googleId, email, publicName })) {
-                return res.status(400).json({ message: 'Missing or invalid parameters in request' });
-            }
-
-            if( await getAccount({googleId: googleId})){
-                return res.status(400).json({ message: 'already has an account' });
-            }
-    
-            const newItem = {
-                googleId: googleId,
-                email: email,
-                publicName: publicName,
-                anonymousName: anonymousName,
-                accountImage: 1,
-                publicPosts: [],
-                anonymousPosts: [],
-                followers: [],
-                following: [],
-                engagements: 0
-            };
-            
-            const result = await collection.insertOne(newItem);
-            res.status(200).json({ insertedId: result.insertedId, ...newItem });
-        } catch (error) {
-            res.status(500).json({ message: 'Error occurred while creating account', error });
+        if (!areAllParametersValid({ googleId, email, publicName, profileImage })) {
+            return res.status(400).json({ message: 'Missing or invalid parameters in request' });
         }
-    });
-    
 
+        const hasAccount = await checkHasAccount({ googleId: googleId });
+        const accountNameTaken = await checkHasAccount({ name: publicName });
+        const isEnrolled = await checkIsEnrolled(googleId);
+
+        if (hasAccount || isEnrolled || accountNameTaken) {
+            return res.status(400).json({ message: 'Account creation not allowed' });
+        }
+
+        const publicAccount = {
+            googleId: googleId,
+            email: email,
+            name: publicName,
+            profileImage: profileImage,
+            posts: [],
+            followers: [],
+            following: [],
+            engagements: 0
+        };
+
+        const anonymousAccount = {
+            googleId: googleId,
+            email: email,
+            name: anonymousName,
+            profileImage: profileImage,
+            posts: [],
+            followers: [],
+            following: [],
+            engagements: 0
+        };
+
+        const enrollAccountResult = await googleIdMapping.insertOne({ googleId: googleId, accounts: [publicName, anonymousName] });
+        if (!enrollAccountResult.acknowledged) {
+            throw new Error('Failed to enroll account');
+        }
+
+        try {
+            const accountsResult = await accounts.insertMany([publicAccount, anonymousAccount]);
+            if (!accountsResult.acknowledged) {
+                // Rollback enrollAccount if accounts insert fails
+                await googleIdMapping.deleteOne({ _id: enrollAccountResult.insertedId });
+                throw new Error('Failed to create accounts');
+            }
+            res.status(200).json({ enrollId: enrollAccountResult.insertedId, accountsId: accountsResult.insertedIds, ...publicAccount, ...anonymousAccount });
+        } catch (accountsError) {
+            // Rollback enrollAccount if any error occurs in accounts creation
+            await googleIdMapping.deleteOne({ _id: enrollAccountResult.insertedId });
+            throw accountsError;
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Error occurred while creating account', error });
+    }
+});
 
 app.get('/api/get-account-info', async (req, res) => {
     try {
-        const {googleId} = req.body;
+        const { name } = req.query;
 
-        if (!googleId) {
-            return res.status(400).json({ message: 'Missing or invalid googleId in request' });
+        if (!name) {
+            return res.status(400).json({ message: 'Missing name parameter' });
         }
 
-        const account = await getAccount({googleId: googleId});
-        res.status(200).json(account);
-
+        const hasAccount = await checkHasAccount({ name: name });
+        
+        if (hasAccount) {
+            const account = await accounts.find({ name: name }).toArray();
+            res.status(200).json(account[0]);
+        } else {
+            res.status(400).json({ message: "Account not found" });
+        }
     } catch (error) {
         res.status(500).json({ message: 'Error occurred while fetching items', error });
     }
 });
 
+
 app.post('/api/create-post', async (req, res) => {
     try {
-        const { title, account, isPublic, postImage, post, googleId} = req.body
+        const { title, name, postImage, post, googleId } = req.body;
 
-        if (!areAllParametersValid({title, account, postImage, post, googleId})) {
+        if (!areAllParametersValid({ title, name, postImage, post, googleId })) {
             return res.status(400).json({ message: 'Missing or invalid parameters in request' });
         }
+
+        const hasAccount = await checkHasAccount({ name: name });
+        if (!hasAccount) {
+            return res.status(400).json({ message: "Account not found" });
+        }
+
         const newItem = {
-            account: account,
+            owner: name,
             ownerId: googleId,
-            isPublic: isPublic,
             title: title,
             post: post,
             postImage: postImage,
             date: new Date(),
             likes: [],
             comments: []
+        };
+
+        const postCreationResponse = await posts.insertOne(newItem);
+        const postId = postCreationResponse.insertedId;
+
+        const accountUpdateResponse = await accounts.updateOne({ name: name }, { $push: { posts: postId } });
+        if (!accountUpdateResponse || accountUpdateResponse.modifiedCount === 0) {
+            // Rollback: Delete the post that was just created
+            await posts.deleteOne({ _id: postId });
+
+            return res.status(500).json({ message: 'Failed to update account, post creation rolled back' });
         }
 
-        const response = await collection.insertOne(newItem);
-        const postId = response.insertedId
-        let response2;
-        const fieldToUpdate = isPublic ? 'publicPosts': 'anonymousPosts'
-        response2 = await collection.updateOne({googleId: googleId}, {$push: {[fieldToUpdate]: postId}});
-
-        res.status(200).json({postID: postId, message2: response2});
+        res.status(200).json({ postID: postId, updateAccount: accountUpdateResponse });
     } catch (error) {
-        res.status(500).json({ message: 'Error occurred while fetching items', error });
-    }   
+        res.status(500).json({ message: 'Error occurred while creating the post', error });
+    }
 });
+
+
 
 app.get('/api/get-post', async (req, res) => {
     try {
-        const {postId} = req.body;
+        console.log(req.query)
+        const {postId} = req.query;
+        
+        if(!postId){
+            return res.status(400).json({ message: 'Missing or invalid parameters in request' });
+        }
         const result = await getPost(postId);
 
-        if (result.success) {
-            res.status(200).json({ success: true, post: result.post });
+        if (result) {
+            res.status(200).json({ success: true, post: result });
         } else {
-            res.status(404).json({ success: false, message: result.message });
+            res.status(404).json({ success: false, message: "post not found" });
         }
     } catch (error) {
-        console.error(error);
         res.status(500).json({ message: 'Error occurred while fetching post', error: error.toString() });
     }
 });
@@ -150,23 +217,32 @@ app.post('/api/delete-post', async (req, res) => {
     try {
         const { postId } = req.body;
         
-        if (!ObjectId.isValid(postId)) {
-            return res.status(400).json({ message: 'Invalid post ID' });
+        if (!postId || !ObjectId.isValid(postId)) {
+            return res.status(400).json({ message: 'Missing or invalid post ID' });
         }
-
+        
         const objectId = new ObjectId(postId);
-        const post = await collection.findOne({_id: objectId});
+        const post = await posts.findOne({_id: objectId});
+        
         if (!post) {
             return res.status(404).json({ message: 'Post not found' });
         }
+        
+        const updateResult = await accounts.updateOne({ name: post.owner }, { $pull: { posts: objectId } });
+        if (!updateResult || updateResult.modifiedCount === 0) {
+            return res.status(404).json({ message: 'Failed to update account posts' });
+        }
 
-        const response1 = await collection.deleteOne({_id: objectId});
-        const fieldToUpdate = post.isPublic ? 'publicPosts' : 'anonymousPosts';
-        const response2 = await collection.updateOne({googleId: post.ownerId}, {$pull: {[fieldToUpdate]: objectId}});
-
-        res.status(200).json({ message: 'Post deleted successfully', success: true, response1, response2 });
+        const deleteResult = await posts.deleteOne({ _id: objectId });
+        if (!deleteResult || deleteResult.deletedCount !== 1) {
+            // Rollback the update on failure to delete post
+            await accounts.updateOne({ name: post.owner }, { $push: { posts: objectId } });
+            return res.status(500).json({ message: 'Failed to delete post' });
+        }
+        
+        res.status(200).json({ message: 'Post deleted successfully' });
     } catch (error) {
-        res.status(500).json({ message: 'Error occurred while deleting post', success: false, error });
+        res.status(500).json({ message: 'Error occurred while deleting post', error });
     }
 });
 
@@ -177,19 +253,17 @@ app.post('/api/like-post', async (req, res) => {
         const {postId, callingAccount, otherAccount, insert} = req.body
         const objectId = new ObjectId(postId);
         const query = await getPost(postId)
-        const post = query.success ? query: res.status(400).json({ message: 'Post not found' });
-        const isPublic = post.isPublic
-        const fieldToUpdate = isPublic ? "publicName": "anonymousName"
+        const post = query ? query: res.status(400).json({ message: 'Post not found' });
         if(!areAllParametersValid({post, callingAccount, otherAccount})){
             return res.status(400).json({ message: 'Missing or invalid parameters in request' });
         }
         let response, response2;
         if(insert){
-            response = await collection.updateOne({_id: objectId}, {$inc: {likes: 1}})
-            response2 = await collection.updateOne({[fieldToUpdate]: otherAccount}, {$inc: {engagments: 1}})
+            response = await posts.updateOne({_id: objectId}, {$inc: {likes: 1}})
+            response2 = await accounts.updateOne({[fieldToUpdate]: otherAccount}, {$inc: {engagments: 1}})
         } else {
-            response = await collection.updateOne({_id: objectId}, {$dec: {likes: 1}})
-            response2 = await collection.updateOne({[fieldToUpdate]: otherAccount}, {$dec: {engagments: 1}})
+            response = await posts.updateOne({_id: objectId}, {$dec: {likes: 1}})
+            response2 = await accounts.updateOne({[fieldToUpdate]: otherAccount}, {$dec: {engagments: 1}})
         }
 
         res.status(200).json([response, response2]);
---
+++ server/Test.js
@@ -0,0 +1,92 @@
+const axios = require('axios');
+
+
+async function createAccount() {
+    try {
+      const response = await axios.post('http://localhost:3000/api/create-account', {
+        googleId: 4,
+        email: "cwdenton5@gmail.com",
+        publicName: "C",
+        profileImage: "1",
+      }, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+  
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error:', error.response ? error.response.data : error.message);
+    }
+  }
+
+async function deletePost() {
+  try {
+    const response = await axios.post('http://localhost:3000/api/delete-post', {
+      postId: "65a9cd1faac867dd8043c979"
+    }, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    console.log(response.data);
+  } catch (error) {
+    console.error('Error:', error.response ? error.response.data : error.message);
+  }
+}
+
+async function createPost() {
+    try {
+      const response = await axios.post('http://localhost:3000/api/create-post', {
+        title: "testing",
+        name: "b0a617355deb1127",
+        postImage: 1,
+        post: 1,
+        googleId: 3
+      }, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+  
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error:', error.response ? error.response.data : error.message);
+    }
+  }
+
+  async function getPost() {
+    try {
+      const response = await axios.get('http://localhost:3000/api/get-post', {
+        params: {
+          postId: "65a9cd98aac867dd8043c97a"
+        }
+      });
+  
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error:', error.response ? error.response.data : error.message);
+    }
+  }
+  
+
+async function getAccountInfo() {
+    try {
+      const response = await axios.get('http://localhost:3000/api/get-account-info', {
+        params: {
+          name: "b0a617355deb1127"
+        },
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error:', error.response ? error.response.data : error.message);
+    }
+}
+
+deletePost();
+
